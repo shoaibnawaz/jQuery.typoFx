181078c878f8b72818c79e19d23cb65c80e7e090
Shuaib Nawaz <shuaibnawaz@gmail.com>
2012-06-05 02:31:04 +0500

    Initial Checkout

---
+++ jquery.typofx.js
@@ -0,0 +1,117 @@
+;(function($){
+    var config = {};
+    var registeredFx = {};
+    $.extend({
+        typoFx: {
+            registerFx : function(fx){
+                registeredFx[fx.name] = fx.fx;
+                config[fx.name] = fx.config;
+            },
+
+            unregisterFx : function(name){
+                delete registeredFx[name];
+                delete config[name];
+            },
+
+            config : function(_conf){
+                if ( typeof(_conf) == 'undefined' )
+                    return config;
+                else
+                    config = _conf;
+            }
+        }
+    });
+
+    $.fn.typoFx = function( _fxname, _config ){
+        if ( typeof(_config) != 'undefined' && $.isPlainObject(_config) ){
+                $.extend(true, config, _config );  
+        }
+        $(this).each(function(){
+            var texts = [];
+            $(this).each(function(i){
+                texts.push(this);
+                (registeredFx[_fxname])(this);
+            });
+        });
+
+        return this;
+    };
+})(jQuery);
+
+jQuery.typoFx.registerFx({ 
+    name: 'faisal', 
+    config: { speed: 1000, delay: 500 }, 
+    fx: function( textNode ){
+        var name = "faisal";
+        var config = jQuery.typoFx.config();
+        var fxconfig = config[name];
+        var $tn = $(textNode);
+        $tn.html( $tn.text( ).replace( /([\S])/g,'<span>$1</span>' ) );
+        $tn.css('position','relative');
+        $('span',$tn).stop().css({position:'relative',
+            opacity:0,
+            fontSize:84,
+            top:function(i){return Math.floor(Math.random()*500)*((i%2)?1:-1);},
+            left:function(i){return Math.floor(Math.random()*500)*((i%2)?1:-1);}
+        }).animate( {opacity:1,fontSize:12,top:0,left:0},fxconfig.speed );
+    }
+});
+
+jQuery.typoFx.registerFx({ 
+    name: 'junaid', 
+    config: { speed: 350, delay: 100, topOffset: -35, leftOffset: 5, fontSize: 84 ,opacity: 0}, 
+    fx: function( textNode ){
+        var name = "junaid";
+        var config = jQuery.typoFx.config();
+        var fxconfig = config[name];
+        var positions = [];   
+        var $tn = $(textNode);
+        $tn.html( $tn.text( ).replace( /([\S])/g,'<span>$1</span>' ) );
+        $tn.css('position','relative');
+        $('span',$tn).each(function(i){
+            positions[i] = $(this).position();
+        }).each(function(i){
+            var initialEffects = {
+                position:'absolute',
+                top: function(){ return positions[i].top + fxconfig.topOffset ; },
+                left: function(){ return positions[i].left + fxconfig.leftOffset ; }
+            };
+            $.extend(true, initialEffects, fxconfig);
+            $(this).stop().css(initialEffects).delay(i*fxconfig.delay).
+                animate( {opacity:1,fontSize:12,top:positions[i].top,left:positions[i].left},fxconfig.speed );
+        });
+    }
+});
+
+jQuery.typoFx.registerFx({
+    name: 'khalid',
+    config: {
+        yOffset: 50,
+        speed: 500,
+        delay: 50,
+        opacity: 0,
+        fontSize: 84
+    },
+    fx: function(textNode){
+        var name = "khalid";
+        var config = jQuery.typoFx.config();
+        var fxconfig = config[name];
+        var $tn = $(textNode);
+        var positions = [];
+        var initialEffects = {};
+        $tn.html( $tn.text( ).replace( /([\S])/g,'<span>$1</span>' ) );
+        $('span',$tn).each(function(i){
+            positions[i] = $(this).position();
+            if( i%2 )
+                $(this).css({top: -1 * fxconfig.yOffset, left: positions[i].left});
+            else
+                $(this).css({top: fxconfig.yOffset, left: positions[i].left});
+            initialEffects.position = 'absolute';
+            $.extend(true, initialEffects, fxconfig);            
+        }).each(function(i){
+            $(this).stop().css(initialEffects).delay(i*fxconfig.delay).
+                animate( {opacity:1,fontSize:12,top:positions[i].top,left:positions[i].left},fxconfig.speed );
+        });
+
+    }
+});
